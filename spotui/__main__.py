a50a43e73907e5736961753352924a0eb628a1b8
ceuk <ceuk.dev@gmail.com>
2020-06-18 22:50:44 +0100

    supress stderr

--- spotui/__main__.py
+++ spotui/__main__.py
@@ -1,9 +1,20 @@
 import curses
+from contextlib import contextmanager, redirect_stderr, redirect_stdout
+from os import devnull
 from spotui.src.ui import App
 
 
+@contextmanager
+def suppress_stdout_stderr():
+    """A context manager that redirects stdout and stderr to devnull"""
+    with open(devnull, 'w') as fnull:
+        with redirect_stderr(fnull) as err, redirect_stdout(fnull) as out:
+            yield (err, out)
+
+
 def main():
-    SpoTUI = curses.wrapper(App)
+    with suppress_stdout_stderr():
+        curses.wrapper(App)
 
 
 if __name__ == "__main__":
