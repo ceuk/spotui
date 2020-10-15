2bba1a7ae0253d7827fb93bb5b5e9c53e74bd4a7
dbkosky <dbkosky@gmail.com>
2020-10-15 03:32:18 +0100

    Fix artist name alignment when track name too long

--- spotui/src/TracksMenu.py
+++ spotui/src/TracksMenu.py
@@ -84,7 +84,6 @@ class TracksMenu(Component):
 
     def __pad_track_text(self, text, max_word_length):
         spaces_needed = max_word_length - len(text)
-        if spaces_needed > 0:
-            for i in range(0, spaces_needed + 1):
-                text += " "
+        for i in range(0, spaces_needed + 1):
+            text += " "
         return text
