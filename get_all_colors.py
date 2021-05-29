8245d98cb4d1fef312937f13e017eca3bf0fa91c
octoshrimpy <octavo.acapella@gmail.com>
2021-05-29 04:28:49 -0600

    beginning of theme engine

---
+++ get_all_colors.py
@@ -0,0 +1,41 @@
+
+import curses
+
+
+def main(stdscr):
+    curses.start_color()
+    curses.use_default_colors()
+
+    count = 1
+
+    for i in range(0, curses.COLORS):
+        curses.init_pair(i + 1, i, -1)
+
+    # try:
+    for i2 in range(0, 255):
+        pad = "{:>3}".format(str(i2))
+        stdscr.addstr(pad, curses.color_pair(i2))
+        stdscr.addstr( "  ")
+
+        if i2 is 0:
+            y, x = stdscr.getyx()
+            maxy, maxx = stdscr.getmaxyx()
+            stdscr.move((y + 1) %maxy, 0)
+            pass
+
+        if i2 is 16 or i2 is 8:
+            y, x = stdscr.getyx()
+            maxy, maxx = stdscr.getmaxyx()
+            stdscr.move((y + 1) %maxy, 0)
+            pass
+
+        if i2 > 16:
+            count = count + 1 
+            if count % 6 is 1:
+                y, x = stdscr.getyx()
+                maxy, maxx = stdscr.getmaxyx()
+                stdscr.move((y + 1) %maxy, 0)
+  
+    stdscr.getch()
+
+curses.wrapper(main)
--- spotui/src/colors.py
+++ spotui/src/colors.py
@@ -20,3 +20,44 @@ def init_colors():
     curses.init_pair(7, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
     # Highlighted (bg)
     curses.init_pair(8, curses.COLOR_WHITE, curses.COLOR_MAGENTA)
+
+    ## testing xterm-256
+
+    # curses.setupterm()
+
+    # # check config value instead of checking here
+    # if curses.tigetnum("colors") == 256:
+        
+    #     # set up individual colors. run get_all_colors.py to see them all
+    #     purple      = 64
+    #     cyan        = 40
+    #     green       = 47
+    #     yellow      = 215
+    #     orange      = 203
+    #     magenta     = 197
+    #     gray_light  = 250
+    #     gray_dark   = 238
+    #     light       = 254
+    #     dark        = 234
+    #     transparent = 0
+
+
+    #     # set up pairs
+    #     # Default text
+    #     curses.init_pair(1, light, transparent)
+    #     # White text
+    #     curses.init_pair(4, light, dark)
+    #     # Yellow text
+    #     curses.init_pair(5, yellow, dark)
+    #     # Magenta text
+    #     curses.init_pair(10, magenta, dark)
+    #     # Green text
+    #     curses.init_pair(11, green, dark)
+    #     # Cyan text
+    #     curses.init_pair(12, cyan, dark)
+    #     # Selected item
+    #     curses.init_pair(6, dark, light)
+    #     # Highlighted (no bg)
+    #     curses.init_pair(7, magenta, dark)
+    #     # Highlighted (bg)
+    #     curses.init_pair(8, light, magenta)
