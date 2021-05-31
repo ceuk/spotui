7bd9edaecd4c6dc2fe2e893ccbad671bc961a7b4
octoshrimpy <octavo.acapella@gmail.com>
2021-05-30 22:00:21 -0600

    transition to theme engine

--- spotui/src/colors.py
+++ spotui/src/colors.py
@@ -8,46 +8,70 @@ def init_colors():
     config = get_config() # symbol use
     use_default_bg = config.get("colors", "use_default_background") == "yes"
     use_256_colors = config.get("colors", "use_256_colors") == "yes"
+    is_default_bg_transparent = config.get("colors", "is_default_bg_transparent") == "yes"
 
     if use_default_bg:
         curses.use_default_colors() # from: https://linux.die.net/man/3/use_default_colors
 
     curses.setupterm()
+
+    #TODO: refactor this into a cleaner if/else where  only vars are iffed, and the init_pair doesn't get repeated
     if use_256_colors and curses.tigetnum("colors") == 256:
         
     #     # set up individual colors. run get_all_colors.py to see them all
-        purple      = 63
-        cyan        = 39
-        green       = 35
-        yellow      = 214
-        orange      = 166
-        magenta     = 203
-        gray_light  = 249
-        gray_dark   = 237
-        light       = 253
-        dark        = 233
-        transparent = -1
 
+        df_fg = int(config.get("custom_colors_256", "default_text_fg"))
+        df_bg = int(config.get("custom_colors_256", "default_text_bg"))
+  
+        ib_fg = int(config.get("custom_colors_256", "inactive_border_fg"))
+        ib_bg = int(config.get("custom_colors_256", "inactive_border_bg"))
+  
+        ab_fg = int(config.get("custom_colors_256", "active_border_fg"))
+        ab_bg = int(config.get("custom_colors_256", "active_border_bg"))
+  
+        pb_fg = int(config.get("custom_colors_256", "popup_border_fg"))
+        pb_bg = int(config.get("custom_colors_256", "popup_border_bg"))
+  
+        sb_fg = int(config.get("custom_colors_256", "seekbar_fg"))
+        sb_bg = int(config.get("custom_colors_256", "seekbar_bg"))
+  
+        lp_fg = int(config.get("custom_colors_256", "list_now_playing_fg"))
+        lp_bg = int(config.get("custom_colors_256", "list_now_playing_bg"))
+  
+        cs_fg = int(config.get("custom_colors_256", "cursor_fg"))
+        cs_bg = int(config.get("custom_colors_256", "cursor_bg"))
+    
+        #TODO: add this functionality
+        cp_fg = int(config.get("custom_colors_256", "cursor_now_playing_fg"))
+        cp_bg = int(config.get("custom_colors_256", "cursor_now_playing_bg"))
 
-        # set up pairs
+        ### set up pairs
         # Default text
-        curses.init_pair(1, light, transparent)
-        # White text - background to inactive component border
-        curses.init_pair(4, light, dark)
-        # Yellow text - background to active component border
-        curses.init_pair(5, yellow, dark)
-        # Magenta text - background to popup components, overrides above
-        curses.init_pair(10, magenta, dark)
-        # Green text - now playing seekbar
-        curses.init_pair(11, green, dark)
-        # Cyan text - currently playing song, in lists
-        curses.init_pair(12, purple, dark)
-        # Selected item - cursor line highlight
-        curses.init_pair(6, dark, light)
-        # Highlighted (no bg) - no use that I can see
-        curses.init_pair(7, magenta, dark)
+        curses.init_pair(1, df_fg, df_bg)
+        
+        # background to inactive component border
+        curses.init_pair(4, ib_fg, ib_bg)
+        
+        # background to active component border
+        curses.init_pair(5, ab_fg, ab_bg)
+        
+        # background to popup components, overrides above
+        curses.init_pair(10, pb_fg, pb_bg)
+        
+        # now playing seekbar
+        curses.init_pair(11, sb_fg, sb_bg)
+        
+        # currently playing song, in lists
+        curses.init_pair(12, lp_fg, lp_bg)
+        
+        # cursor line highlight
+        curses.init_pair(6, cs_fg, cs_bg)
+
+        # cursor line highlight, list item selected, not implemented
+        curses.init_pair(7, cp_fg, cp_bg)
+
         # Highlighted (bg) - no use that I can see
-        curses.init_pair(8, light, magenta)
+        #curses.init_pair(8, light, magenta)
     
     else:
         curses.init_pair(1, curses.COLOR_WHITE, 0)
